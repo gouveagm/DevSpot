75db338957d31989ae12388f18ecdbc19d137d40
Gabriel <gabriel.mendes-gouvea@capgemini.com>
2025-06-20 10:16:04 +0100

    Add JobPosting with migration

---
+++ DevSpot/Migrations/20250620090523_AddedJobPosting.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DevSpot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedJobPosting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobPostings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    CompanyName = table.Column<string>(type: "text", nullable: false),
+                    PostedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsApproved = table.Column<bool>(type: "boolean", nullable: false),
+                    PostedById = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobPostings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobPostings_AspNetUsers_PostedById",
+                        column: x => x.PostedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobPostings_PostedById",
+                table: "JobPostings",
+                column: "PostedById");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobPostings");
+        }
+    }
+}
---
+++ DevSpot/Models/JobPosting.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevSpot.Models;
+
+public class JobPosting
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public string Location { get; set; }
+    [Required]
+    public string CompanyName { get; set; }
+    public DateTime PostedDate { get; set; } = DateTime.UtcNow;
+    public bool IsApproved { get; set; }
+    [Required]
+    public string PostedById { get; set; }
+
+    [ForeignKey(nameof(PostedById))]
+    public IdentityUser User { get; set; }
+}
